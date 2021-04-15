7015869fe58114f81e3c810b016955289a5abfc3
Andreas Mårtensson <andreas@addem.se>
2021-04-15 13:38:18 +0200

    configmaps RBAC

--- controllers/templatesecret_controller.go
+++ controllers/templatesecret_controller.go
@@ -48,16 +48,8 @@ func (r *TemplateSecretReconciler) Scheme() *runtime.Scheme {
 //+kubebuilder:rbac:groups=addem.se,resources=templatesecrets/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=addem.se,resources=templatesecrets/finalizers,verbs=update
 //+kubebuilder:rbac:groups="",resources=secrets,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups="",resources=configmaps,verbs=get;list;watch
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the TemplateSecret object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.7.0/pkg/reconcile
 func (r *TemplateSecretReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := r.Log.WithValues("templatesecret", req.NamespacedName)
 
@@ -212,9 +204,9 @@ func getTemplate(ctx context.Context, r *TemplateSecretReconciler, tSecret api.T
 	}
 }
 
-// SetupWithManager sets up the controller with the Manager.
 func (r *TemplateSecretReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&api.TemplateSecret{}).
+		Owns(&corev1.Secret{}).
 		Complete(r)
 }
